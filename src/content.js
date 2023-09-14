1e3d33fe9e8947125d93920cd1a97cf5f9eb5e2d
Christian Bird <cbird@microsoft.com>
2023-09-14 05:48:47 -0700

    added and extracted styles

--- src/content.js
+++ src/content.js
@@ -6,55 +6,13 @@ function appendElements(selector) {
     if (!targetElement) return;
 
     html = `
-    <style>
-    #floatingDiv {
-        position: fixed;
-        top: 0;
-        left: 50%;
-        transform: translateX(-50%);
-        z-index: 1000;
-        background-color: white;
-        padding: 15px 20px;
-        border-bottom: 2px solid #ccc;
-        width: auto;
-        box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);
-        font-family: 'Arial', sans-serif; /* A more readable font */
-        border-radius: 0 0 5px 5px; /* Rounded corners at the bottom */
-    }
-
-    #floatingDiv label,
-    #floatingDiv input,
-    #floatingDiv span {
-        margin-right: 10px; /* Spacing between elements */
-    }
-
-    #thresholdInput {
-        padding: 5px;
-        border: 1px solid #ccc;
-        border-radius: 3px; /* Slightly rounded corners for the input */
-        transition: border 0.3s; /* Smooth transition for hover effects */
-    }
-
-    #thresholdInput:hover {
-        border-color: #999; /* Change border color on hover */
-    }
-
-    #thresholdInput:focus {
-        border-color: #666; /* Change border color on focus */
-        outline: none; /* Remove the default browser outline */
-    }
-</style>
-
-<div id="floatingDiv">
-    <label for="thresholdInput">Preference cut-off (inclusive): </label>
-    <input type="number" id="thresholdInput" value="10">
-    <span>Count:<span id="countText"/></span>
-</div>
-
+        <div id="floatingDiv">
+            <label for="thresholdInput">Preference cut-off (inclusive): </label>
+            <input type="number" id="thresholdInput" value="10">
+            <span>Count:<span id="countText"/></span>
+        </div>
     `;
     targetElement.insertAdjacentHTML('afterend', html);
-
-    // Create a text element (span) to display the count
     const numberInput = document.getElementById('thresholdInput');
 
     // Add event listener to the number input to recompute the count when its value changes
@@ -62,9 +20,10 @@ function appendElements(selector) {
 
 }
 
-// Modified function to use the value from the number input as the threshold
 function countInputsGreaterThanThreshold() {
     const threshold = parseInt(document.getElementById('thresholdInput').value, 10) || 10;
+    
+    //grab all the input cells in the preferences page
     let inputs = document.querySelectorAll('input.revpref');
     let count = 0;
 
