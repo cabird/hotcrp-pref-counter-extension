71714d29aa2a16f814f680ca87f9fc9d54f12b14
Christian Bird <cbird@microsoft.com>
2023-09-19 13:52:52 -0700

    added a table of preferences

--- src/content.js
+++ src/content.js
@@ -8,6 +8,149 @@ function getThreshold(callback) {
     chrome.runtime.sendMessage({ method: 'setThreshold', threshold: threshold });  
   }  
 
+floatingDivHtml = `
+<!-- floatingDiv.html -->
+<div id="floatingDiv">
+    <!-- Summary (initially visible) -->
+    <div id="summary">
+        <label for="thresholdInput">Preference cut-off (inclusive): </label>
+        <input type="number" id="thresholdInput" value="10">
+        <span>Count: <span id="countText"/></span>
+        <button id="toggleButton">Show Details</button> <!-- Button to toggle table visibility -->
+    </div>
+    <!-- Table (initially hidden) -->
+    <div id="countTableContainer" style="display: none;">
+        <!-- Table content will be rendered here -->
+    </div>
+</div>
+
+`
+
+
+// Function to load and insert the HTML content from an external file
+async function loadFloatingDivStructure() {
+    try {
+        const html = floatingDivHtml;
+        const selector = '#f-search';
+        const targetElement = document.querySelector(selector);
+        if (!targetElement) return;
+        targetElement.insertAdjacentHTML('afterend', html);
+
+        // Add a click event listener to the toggle button
+        const toggleButton = document.getElementById('toggleButton');
+        toggleButton.addEventListener('click', toggleTableVisibility);
+    } catch (error) {
+        console.error('Error loading HTML:', error);
+    }
+}
+
+// Call the function to load the HTML content during initialization
+loadFloatingDivStructure();//.then( () =>
+//{
+//    // Add a click event listener to the toggle button
+//    const toggleButton = document.getElementById('toggleButton');
+//    toggleButton.addEventListener('click', toggleTableVisibility);
+//}
+//);
+
+// Function to toggle the visibility of the table
+function toggleTableVisibility() {
+    const tableContainer = document.getElementById('countTableContainer');
+    if (tableContainer.style.display === 'none') {
+        tableContainer.style.display = 'block';
+        document.getElementById('toggleButton').textContent = 'Hide Details';
+    } else {
+        tableContainer.style.display = 'none';
+        document.getElementById('toggleButton').textContent = 'Show Details';
+    }
+}
+
+
+
+// Data structure to store preference counts
+const preferenceCounts = {};
+
+// Function to update the counts and display them
+function updatePreferenceCounts() {
+    console.log("updating preference counts...");
+    // Reset counts
+    Object.keys(preferenceCounts).forEach(key => preferenceCounts[key] = 0);
+
+    // Collect counts from preferences on the page
+    const preferences = document.querySelectorAll('input.revpref');
+    preferences.forEach(input => {
+        const value = parseInt(input.value, 10);
+        if (!isNaN(value)) {
+            if (!preferenceCounts[value]) {
+                preferenceCounts[value] = 1;
+            } else {
+                preferenceCounts[value]++;
+            }
+        }
+    });
+
+    //output the preference counts
+    console.log(`preference counts ${preferenceCounts}`);
+    for (const value in preferenceCounts)
+    {
+        console.log(`pref ${value} : ${preferenceCounts[value]}`);
+    }
+
+    renderCountsAsTable(preferenceCounts);
+    // Update the UI to display the counts
+    // You can choose the format (e.g., a table) to display the counts here
+    // Example: renderCountsAsTable(preferenceCounts);
+}
+
+// Function to render the counts as a table (example)
+function renderCountsAsTable(counts) {
+    const table = document.createElement('table');
+    table.id = 'PrefTable';
+    // Create thead element
+    const thead = document.createElement('thead');
+    table.appendChild(thead);
+
+    // Set the innerHTML of the thead
+    thead.innerHTML = `
+        <tr>
+            <th>Pref</th>
+            <th>Count</th>
+        </tr>
+    `;
+    const tbody = document.createElement('tbody');
+    
+    for (const value in counts) {
+        const row = document.createElement('tr');
+        const cellValue = document.createElement('td');
+        const cellCount = document.createElement('td');
+        
+        cellValue.textContent = value;
+        cellCount.textContent = counts[value];
+        
+        row.appendChild(cellValue);
+        row.appendChild(cellCount);
+        tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
+    
+    // Clear existing UI and append the new table
+    const countTextElement = document.getElementById('countTableContainer');
+    countTextElement.innerHTML = '';
+    countTextElement.appendChild(table);
+}
+
+// Add an event listener to recalculate counts when preferences change
+let inputs = document.querySelectorAll('input.revpref');
+inputs.forEach(input => {
+    input.addEventListener('input', updatePreferenceCounts);
+});
+
+// Call updatePreferenceCounts during initialization
+updatePreferenceCounts();
+
+
+
+
 // Function to append a number input and a text element to a given HTML element
 function appendElements(selector) {
     const targetElement = document.querySelector(selector);
@@ -59,8 +202,8 @@ function countInputsGreaterThanThreshold() {
 }
 
 // Attach event listeners to each input element
-let inputs = document.querySelectorAll('input.revpref');
-inputs.forEach(input => {
+let inputs2 = document.querySelectorAll('input.revpref');
+inputs2.forEach(input => {
     input.addEventListener('input', countInputsGreaterThanThreshold);
 });
 
